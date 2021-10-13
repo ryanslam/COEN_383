88eddde3200e85a19936d72bbf2da3df5804aaf3
Alex <alexle5675@yahoo.com>
2021-10-13 14:12:17 -0700

    Update skeleton_code.cpp
    updating it to make code actually correct

--- project_3/skeleton_code.cpp
+++ project_3/skeleton_code.cpp
@@ -6,7 +6,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 // seller thread to serve one time slice (1 minute)
 void * sell(char *seller_type)
 {
-    While (having more work todo)
+    while (having more work todo)
     {
         pthread_mutex_lock(&mutex);
         pthread_cond_wait(&cond, &mutex);
@@ -26,26 +26,26 @@ int main()
 {
     int i;
     pthread_t tids[10];
-    char Seller_type;
+    char seller_type;
     // Create necessary data structures for the simulator.
     // Create buyers list for each seller ticket queue based on the
     // N value within an hour and have them in the seller queue.
     // Create 10 threads representing the 10 sellers.
-    seller-type = “H”;
-    pthread_create(&tids[0], NULL, sell, &seller-type);
-    seller-type = “M”;
+    seller_type = 'H';
+    pthread_create(&tids[0], NULL, sell , &seller_type);
+    seller_type = 'M';
     for (i = 1; i < 4; i++)
-    pthread_create(&tids[i], NULL, sell, &seller-type);
-    seller-type = “L”;
+        pthread_create(&tids[i], NULL, sell , &seller_type);
+    seller_type = 'L';
     for (i = 4; i < 10; i++)
-    pthread_create(&tids[i], NULL, sell, &seller-type);
+        pthread_create(&tids[i], NULL, sell, &seller_type);
     // wakeup all seller threads
     wakeup_all_seller_threads();
 
     // wait for all seller threads to exit
     for (i = 0 ; i < 10 ; i++)
-    Pthread_join(&tids[i], NULL);
+    pthread_join(&tids[i], NULL);
+
     // Printout simulation results
-    …………
-    exit(0);
+    return;
 }
